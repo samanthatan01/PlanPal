67df1829313169e911bcc0d9c7df80c55b249e4a
samanthatan01 <samanthatan6496@gmail.com>
2024-04-23 15:25:34 +0800

    Create routers, middleware and controller folders

---
+++ Backend/src/controllers/auth.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+const pool = require("../db/db");
+
+const seedUsers = async (req, res) => {
+  try {
+  } catch (error) {}
+};
