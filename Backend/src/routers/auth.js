6bebe8edd826143a3b5fe4ef09a9fcbd2b9ad593
samanthatan01 <samanthatan6496@gmail.com>
2024-04-24 15:12:26 +0800

    Update router auth.js

--- Backend/src/routers/auth.js
+++ Backend/src/routers/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+const { register, login, refresh } = require("../controllers/auth");
 
 const router = express.Router();
 
 router.put("/register", register);
 router.post("/login", login);
+router.post("/refresh", refresh);
 
 module.exports = router;
