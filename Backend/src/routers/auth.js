85939473ba4663103cba5d14918448600556cb39
samanthatan01 <samanthatan6496@gmail.com>
2024-04-25 10:59:22 +0800

    Update router middleware for auth.js

--- Backend/src/routers/auth.js
+++ Backend/src/routers/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { register, login, refresh } = require("../controllers/auth");
+const { user } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.put("/register", register);
 router.post("/login", login);
-router.post("/refresh", refresh);
+router.post("/refresh", user, refresh);
 
 module.exports = router;
