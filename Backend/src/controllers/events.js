1e19008c861cd8436696d3da1cb9fa88b6ad256f
samanthatan01 <samanthatan6496@gmail.com>
2024-04-25 10:59:35 +0800

    Update controller and router for events.js

---
+++ Backend/src/controllers/events.js
@@ -0,0 +1,111 @@
+const pool = require("../db/db");
+
+const createEvent = async (req, res) => {
+  try {
+    const client = await pool.connect();
+
+    const { title, date, time, address, response_deadline } = req.body;
+    const host_id = req.decoded.id;
+
+    await client.query(
+      "INSERT INTO events (title, date, time, address, response_deadline, host_id) VALUES ($1, $2, $3, $4, $5, $6)",
+      [title, date, time, address, response_deadline, host_id]
+    );
+
+    client.release();
+    res.status(200).json({ status: "ok", msg: `event created` });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "fail to create new event" });
+  }
+};
+
+const getAllEventsHostedByUser = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const host_id = req.decoded.id;
+    const eventsCreated = await client.query(
+      "SELECT * FROM events WHERE host_id = ($1)",
+      [host_id]
+    );
+    client.release();
+    // console.log(eventsCreated);
+    res.status(200).json(eventsCreated.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "fail to retrieve event" });
+  }
+};
+
+const updateEvent = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    // define personnel_id of logged in user
+    const loggedInUserId = req.decoded.id;
+
+    // retrieve event data by id
+    const event_id = parseInt(req.params.id);
+
+    const eventData = await client.query(
+      "SELECT * FROM events WHERE event_id = $1",
+      [event_id]
+    );
+
+    // if personnel_id of logged in user is not the host_id of the event, return unauthorised message. else proceed to update event
+    if (eventData.rows[0].host_id != loggedInUserId) {
+      res.status(401).json({ status: "error", msg: "unauthorised user" });
+    }
+
+    const { title, date, time, address, response_deadline } = req.body;
+    await client.query(
+      "UPDATE events SET title = $1, date = $2, time = $3, address = $4, response_deadline = $5 WHERE event_id = $6",
+      [title, date, time, address, response_deadline, event_id]
+    );
+
+    client.release();
+    res.status(200).json({ status: "ok", msg: "event updated successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "fail to update event" });
+  }
+};
+
+const deleteEvent = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    // define personnel_id of logged in user
+    const loggedInUserId = req.decoded.id;
+
+    // retrieve event data by id
+    const event_id = parseInt(req.params.id);
+
+    const eventData = await client.query(
+      "SELECT * FROM events WHERE event_id = $1",
+      [event_id]
+    );
+
+    // if personnel_id of logged in user is not the host_id of the event, return unauthorised message. else proceed to update event
+    if (eventData.rows[0].host_id != loggedInUserId) {
+      res.status(401).json({ status: "error", msg: "unauthorised user" });
+    }
+
+    const { is_active } = req.body;
+    await client.query("UPDATE events SET is_active = $1 WHERE event_id = $2", [
+      is_active,
+      event_id,
+    ]);
+
+    client.release();
+    res.status(200).json({ status: "ok", msg: "event deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "fail to update event" });
+  }
+};
+
+module.exports = {
+  createEvent,
+  getAllEventsHostedByUser,
+  updateEvent,
+  deleteEvent,
+};
--- Backend/src/routers/events.js
+++ Backend/src/routers/events.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const {
+  createEvent,
+  getAllEventsHostedByUser,
+  updateEvent,
+  deleteEvent,
+} = require("../controllers/events");
+const { user } = require("../middleware/auth");
 
 const router = express.Router();
+
+router.put("/create", user, createEvent);
+router.get("/all", user, getAllEventsHostedByUser);
+router.patch("/update/:id", user, updateEvent);
+router.delete("/delete/:id", user, deleteEvent);
 
 module.exports = router;
