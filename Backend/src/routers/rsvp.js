fe363be209d74156b136d96e46e4868dfeab325b
samanthatan01 <samanthatan6496@gmail.com>
2024-04-26 18:58:58 +0800

    Update rsvp.js with validators

--- Backend/src/routers/rsvp.js
+++ Backend/src/routers/rsvp.js
@@ -6,11 +6,30 @@ const {
   getEventsForGuest,
   updateResponse,
 } = require("../controllers/rsvp");
+const {
+  validateIdInParam,
+  validateResponseInput,
+} = require("../validators/rsvp");
+const { errorCheck } = require("../validators/errorCheck");
 
 const router = express.Router();
 
-router.put("/submit/:id", user, submitResponse);
+router.put(
+  "/submit/:id",
+  user,
+  validateIdInParam,
+  validateResponseInput,
+  errorCheck,
+  submitResponse
+);
 router.get("/", user, getEventsForGuest);
-router.patch("/update/:id", user, updateResponse);
+router.patch(
+  "/update/:id",
+  user,
+  validateIdInParam,
+  validateResponseInput,
+  errorCheck,
+  updateResponse
+);
 
 module.exports = router;
