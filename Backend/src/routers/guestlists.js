334589873a0d59f17709615dcffeb292344605d3
samanthatan01 <samanthatan6496@gmail.com>
2024-04-26 18:58:43 +0800

    Update guestlists.js with validators

--- Backend/src/routers/guestlists.js
+++ Backend/src/routers/guestlists.js
@@ -7,12 +7,41 @@ const {
   updateGuestInfo,
   deleteGuestFromEvent,
 } = require("../controllers/guestlists");
+const {
+  validateIdInParam,
+  validateIdInBody,
+  validateAttendanceUpdate,
+  validateGuestInfoUpdate,
+  validateRemoveGuest,
+} = require("../validators/guestlists");
+const { errorCheck } = require("../validators/errorCheck");
 
 const router = express.Router();
 
-router.get("/:id", user, getGuestlist);
-router.patch("/update-attendance", user, updateGuestAttendance);
-router.patch("/update-information", user, updateGuestInfo);
-router.delete("/delete-guest", user, deleteGuestFromEvent);
+router.get("/:id", user, validateIdInParam, errorCheck, getGuestlist);
+router.patch(
+  "/update-attendance",
+  user,
+  validateIdInBody,
+  validateAttendanceUpdate,
+  errorCheck,
+  updateGuestAttendance
+);
+router.patch(
+  "/update-information",
+  user,
+  validateIdInBody,
+  validateGuestInfoUpdate,
+  errorCheck,
+  updateGuestInfo
+);
+router.delete(
+  "/delete-guest",
+  user,
+  validateIdInBody,
+  validateRemoveGuest,
+  errorCheck,
+  deleteGuestFromEvent
+);
 
 module.exports = router;
