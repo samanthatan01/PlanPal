33bd4d121cc34f2d67100be7c63395aee4901f98
samanthatan01 <samanthatan6496@gmail.com>
2024-04-26 18:58:14 +0800

    Update auth.js with validators

--- Backend/src/routers/auth.js
+++ Backend/src/routers/auth.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { register, login, refresh } = require("../controllers/auth");
+const {
+  register,
+  login,
+  refresh,
+  updateUserInfo,
+} = require("../controllers/auth");
+const { errorCheck } = require("../validators/errorCheck");
 const { user } = require("../middleware/auth");
+const {
+  validateLogin,
+  validateRefresh,
+  validateAccountInfo,
+} = require("../validators/auth");
 
 const router = express.Router();
 
-router.put("/register", register);
-router.post("/login", login);
-router.post("/refresh", user, refresh);
+router.put("/register", validateAccountInfo, errorCheck, register);
+router.post("/login", validateLogin, errorCheck, login);
+router.post("/refresh", user, validateRefresh, errorCheck, refresh);
+router.patch("/update", user, validateAccountInfo, errorCheck, updateUserInfo);
 
 module.exports = router;
