edece575c440fdaaa0e06086a031d48b0c84aa1c
samanthatan01 <samanthatan6496@gmail.com>
2024-04-26 18:57:42 +0800

    Update auth.js
    new route to update user account

--- Backend/src/controllers/auth.js
+++ Backend/src/controllers/auth.js
@@ -111,4 +111,43 @@ const refresh = async (req, res) => {
   }
 };
 
-module.exports = { register, login, refresh };
+const updateUserInfo = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    // find if the email already exists in the db
+    const { email } = req.body;
+    const loggedInUserId = req.decoded.id;
+    const checkDuplicateEmail = await client.query(
+      "SELECT * FROM personnel WHERE email = ($1)",
+      [email]
+    );
+    // if user email is true, return 'email already exists'
+    if (checkDuplicateEmail.rowCount) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "email already exists" });
+    }
+
+    const hash = await bcrypt.hash(req.body.password, 12);
+
+    const { first_name, last_name, contact, diet } = req.body;
+
+    await client.query(
+      "UPDATE personnel SET first_name = $1, last_name = $2, password = $3, email = $4, contact = $5, diet = $6 WHERE personnel_id = $7 ",
+      [first_name, last_name, hash, email, contact, diet, loggedInUserId]
+    );
+
+    client.release();
+    res.status(200).json({
+      status: "ok",
+      msg: `account information has been updated successfully`,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "fail to update information" });
+  }
+};
+
+module.exports = { register, login, refresh, updateUserInfo };
