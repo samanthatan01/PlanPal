3b9c694ae7a03843ddb9c50fa1dd6ce8eac1c110
samanthatan01 <samanthatan6496@gmail.com>
2024-04-29 16:51:47 +0800

    Update Main.jsx

--- Frontend/src/pages/Main.jsx
+++ Frontend/src/pages/Main.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "../components/Main.module.css";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Main = () => {
-  return <div>Main landing page for General Users</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className="col-md-3"></div>
+        <div className={`col-md-6 ${styles.header}`}>
+          HOST YOUR EVENTS <br />
+          MANAGE YOUR RSVP <br />
+          <p className={`${styles.text}`}>all-in-one-place</p>
+          <NavLink
+            style={{ textDecoration: "none", borderBottom: "none" }}
+            className={(navData) => (navData.isActive ? styles.active : "")}
+            to="/login"
+          >
+            <button className={styles.Button}>Get Started</button>
+          </NavLink>
+        </div>
+        <div className="col-md-3"></div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
