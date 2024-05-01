506968bbf4947913f24124c349b0fe7f19928d66
samanthatan01 <samanthatan6496@gmail.com>
2024-05-01 09:38:57 +0800

    Update EventListing.jsx
    Add update modal

--- Frontend/src/components/EventListing.jsx
+++ Frontend/src/components/EventListing.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../components/Events.module.css";
+import UpdateModal from "./UpdateModal";
 
 const EventListing = (props) => {
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
   return (
     <>
+      {showUpdateModal && (
+        <UpdateModal
+          id={props.id}
+          title={props.title}
+          date={props.date}
+          time={props.time}
+          address={props.address}
+          deadline={props.deadline}
+          status={props.status}
+          getAllEvents={props.getAllEvents}
+          setShowUpdateModal={setShowUpdateModal}
+        />
+      )}
       <div className={`${styles.box}`}>
         <h3>{props.title}</h3>
         <p>{props.id}</p>
@@ -11,8 +26,16 @@ const EventListing = (props) => {
         <p>{props.time}</p>
         <p>{props.address}</p>
 
-        <button className={`${styles.btn}`}>EDIT</button>
-        <button className={`${styles.btn}`}>VIEW</button>
+        <button className={`${styles.btn}`}>VIEW EVENT</button>
+        <br />
+        <button
+          className={`${styles.btn}`}
+          onClick={() => setShowUpdateModal(true)}
+        >
+          UPDATE
+        </button>
+        <br />
+        <button className={`${styles.btn}`}>ALL GUESTS</button>
       </div>
     </>
   );
