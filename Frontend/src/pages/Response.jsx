3ddc7543e4fe30f564ce639b9c7b98ad44f1ac03
samanthatan01 <samanthatan6496@gmail.com>
2024-05-02 08:34:59 +0800

    Update Response.jsx
    create searchbar

--- Frontend/src/pages/Response.jsx
+++ Frontend/src/pages/Response.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../components/SideBar";
+import styles from "../components/Events.module.css";
+import UserContext from "../context/user";
+import useFetch from "../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const Response = () => {
+  const [searchId, setSearchId] = useState("");
+  const navigate = useNavigate();
+
+  const goToEvent = () => {
+    navigate("/events/" + searchId);
+  };
+
   return (
     <>
       <div style={{ display: "flex" }}>
         <SideBar></SideBar>
         <main style={{ flexGrow: 1, padding: "20px" }}>
-          <div>User to search event by ID, view event and submit RSVP</div>
+          <>
+            <div>
+              <br />
+              <strong> SEARCH FOR EVENT TO RSVP</strong>
+            </div>
+            <div className={`${styles.wrapper}`}>
+              <input
+                placeholder="Enter event id"
+                className={`${styles.searchbar}`}
+                onChange={(e) => {
+                  setSearchId(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <button className={`${styles.goBtn}`} onClick={() => goToEvent()}>
+                GO
+              </button>
+            </div>
+          </>
         </main>
       </div>
     </>
